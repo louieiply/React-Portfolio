5d6e6f1b92be265a27c63071dfe759c7337a7506
louieiply <louieiply@gmail.com>
2022-07-31 16:48:09 +0930

    Initialise Component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Portfolio from './components/Pages/Portfolio';
+function App() {
+  return (
+    <div className="Main">
+      <Portfolio/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Pages/Portfolio.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Portfolio() {
+    return (
+        <div>
+            <p>
+                Hello World!!!
+            </p>
+        </div>
+    )
+}
+
+export default Portfolio;
